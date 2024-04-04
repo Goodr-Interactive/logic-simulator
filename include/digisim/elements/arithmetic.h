c69e01dab04f016380653127bcc61e25c0a7da2c
Taylor Whatley <32211852+1whatleytay@users.noreply.github.com>
2024-04-04 05:57:49 -0400

    Add arithmetic tests

--- include/digisim/elements/arithmetic.h
+++ include/digisim/elements/arithmetic.h
@@ -1,34 +1,93 @@
+/**
+* @file
+* @brief Arithmetic Component (Addition, Subtraction, Multiplication)
+*/
+
 #ifndef DIGISIM_ELEMENTS_ARITHMETIC_H
 #define DIGISIM_ELEMENTS_ARITHMETIC_H
 
 #include <digisim/element.h>
 #include <digisim/terminal.h>
 
+/**
+ * The operation for the arithmetic element.
+ */
 typedef enum di_arithmetic_op_t {
+    /**
+     * Adds both inputs together.
+     */
     DI_ARITHMETIC_OP_ADD,
+    /**
+     * Subtracts the second input from the first.
+     */
     DI_ARITHMETIC_OP_SUB,
+    /**
+     * Multiplies both inputs together (unsigned).
+     */
     DI_ARITHMETIC_OP_MUL,
+    /**
+     * Multiplies both inputs together (signed).
+     */
     DI_ARITHMETIC_OP_MUL_SIGNED,
     // DIV is more complicated, temporarily unsupported
 } DiArithmeticOp;
 
+/**
+ * Arithmetic element for math.
+ *
+ * To initialize/destroy, use di_arithmetic_init/di_arithmetic_destroy.
+ */
 typedef struct di_arithmetic_t {
+    /**
+     * Element descriptor
+     */
     DiElement element;
 
+    /**
+     * Bit width for inputs and outputs.
+     */
     size_t bits;
 
+    /**
+     * The operation to perform on the inputs.
+     */
     DiArithmeticOp op;
 
+    /**
+     * First input
+     */
     DiTerminal in_a;
+
+    /**
+     * Second input
+     */
     DiTerminal in_b;
 
     // TODO: Implement carry bits.
     // Detecting overflow in C is fairly challenging.
 
+    /**
+     * Output, determined by the operation `op` and both inputs.
+     */
     DiTerminal output;
 } DiArithmetic;
 
+/**
+ * Initialize a DiArithmetic struct.
+ *
+ * @memberof DiArithmetic
+ * @param arithmetic Pointer to initialize
+ * @param bits Bit-size for the input/output terminals
+ * @param op The operation to perform on the inputs
+ */
 void di_arithmetic_init(DiArithmetic *arithmetic, size_t bits, DiArithmeticOp op);
+
+/**
+ * Destroy a DiArithmetic struct.
+ *
+ * @memberof DiArithmetic
+ * @param arithmetic Pointer to destroy
+ */
 void di_arithmetic_destroy(DiArithmetic *arithmetic);
 
 #endif // DIGISIM_ELEMENTS_ARITHMETIC_H
--- tests/test_arithmetic.cpp
+++ tests/test_arithmetic.cpp
@@ -7,6 +7,8 @@ extern "C" {
 #include <digisim/simulation.h>
 }
 
+#include <utils/bit_string.h>
+
 TEST_CASE("Test Constant Has Value") {
     DiOutput out;
     DiNode valueToOut;
@@ -34,4 +36,256 @@ TEST_CASE("Test Constant Has Value") {
 
     di_output_destroy(&out);
     di_constant_destroy(&value);
+}
+
+TEST_CASE("Test Arithmetic Add") {
+    DiInput a;
+    DiInput b;
+
+    DiArithmetic math;
+
+    DiOutput o;
+
+    DiNode aToMath;
+    DiNode bToMath;
+
+    DiNode mathToO;
+
+    di_input_init(&a, 8);
+    di_input_init(&b, 8);
+    di_arithmetic_init(&math, 8, DI_ARITHMETIC_OP_ADD);
+    di_output_init(&o, 8);
+
+    di_node_init(&aToMath, 8);
+    di_node_init(&bToMath, 8);
+    di_node_init(&mathToO, 8);
+
+    di_connect(&aToMath, &a.output);
+    di_connect(&aToMath, &math.in_a);
+
+    di_connect(&bToMath, &b.output);
+    di_connect(&bToMath, &math.in_b);
+
+    di_connect(&mathToO, &math.output);
+    di_connect(&mathToO, &o.input);
+
+    DiSimulation *simulation = di_simulation_create();
+
+    DiSignal signal1;
+    DiSignal signal2;
+
+    di_signal_init(&signal1, 8);
+    di_signal_init(&signal2, 8);
+
+    bit_string::copy_bit_string_to_signal(&signal1, "01101111");
+    bit_string::copy_bit_string_to_signal(&signal2, "00010001");
+
+    di_input_set(&a, signal1, simulation);
+    di_input_set(&b, signal2, simulation);
+
+    di_simulation_run(simulation, 100);
+
+    auto value = bit_string::signal_to_bit_string(&o.signal);
+
+    REQUIRE(value == "10000000");
+
+    di_simulation_free(simulation);
+
+    di_node_destroy(&aToMath);
+    di_node_destroy(&bToMath);
+    di_node_destroy(&mathToO);
+
+    di_input_destroy(&a);
+    di_input_destroy(&b);
+    di_arithmetic_destroy(&math);
+    di_output_destroy(&o);
+}
+
+TEST_CASE("Test Arithmetic Sub") {
+    DiInput a;
+    DiInput b;
+
+    DiArithmetic math;
+
+    DiOutput o;
+
+    DiNode aToMath;
+    DiNode bToMath;
+
+    DiNode mathToO;
+
+    di_input_init(&a, 8);
+    di_input_init(&b, 8);
+    di_arithmetic_init(&math, 8, DI_ARITHMETIC_OP_SUB);
+    di_output_init(&o, 8);
+
+    di_node_init(&aToMath, 8);
+    di_node_init(&bToMath, 8);
+    di_node_init(&mathToO, 8);
+
+    di_connect(&aToMath, &a.output);
+    di_connect(&aToMath, &math.in_a);
+
+    di_connect(&bToMath, &b.output);
+    di_connect(&bToMath, &math.in_b);
+
+    di_connect(&mathToO, &math.output);
+    di_connect(&mathToO, &o.input);
+
+    DiSimulation *simulation = di_simulation_create();
+
+    DiSignal signal1;
+    DiSignal signal2;
+
+    di_signal_init(&signal1, 8);
+    di_signal_init(&signal2, 8);
+
+    bit_string::copy_bit_string_to_signal(&signal1, "01101111");
+    bit_string::copy_bit_string_to_signal(&signal2, "00010001");
+
+    di_input_set(&a, signal1, simulation);
+    di_input_set(&b, signal2, simulation);
+
+    di_simulation_run(simulation, 100);
+
+    auto value = bit_string::signal_to_bit_string(&o.signal);
+
+    REQUIRE(value == "01011110");
+
+    di_simulation_free(simulation);
+
+    di_node_destroy(&aToMath);
+    di_node_destroy(&bToMath);
+    di_node_destroy(&mathToO);
+
+    di_input_destroy(&a);
+    di_input_destroy(&b);
+    di_arithmetic_destroy(&math);
+    di_output_destroy(&o);
+}
+
+TEST_CASE("Test Arithmetic Mul") {
+    DiInput a;
+    DiInput b;
+
+    DiArithmetic math;
+
+    DiOutput o;
+
+    DiNode aToMath;
+    DiNode bToMath;
+
+    DiNode mathToO;
+
+    di_input_init(&a, 8);
+    di_input_init(&b, 8);
+    di_arithmetic_init(&math, 8, DI_ARITHMETIC_OP_MUL);
+    di_output_init(&o, 8);
+
+    di_node_init(&aToMath, 8);
+    di_node_init(&bToMath, 8);
+    di_node_init(&mathToO, 8);
+
+    di_connect(&aToMath, &a.output);
+    di_connect(&aToMath, &math.in_a);
+
+    di_connect(&bToMath, &b.output);
+    di_connect(&bToMath, &math.in_b);
+
+    di_connect(&mathToO, &math.output);
+    di_connect(&mathToO, &o.input);
+
+    DiSimulation *simulation = di_simulation_create();
+
+    DiSignal signal1;
+    DiSignal signal2;
+
+    di_signal_init(&signal1, 8);
+    di_signal_init(&signal2, 8);
+
+    bit_string::copy_bit_string_to_signal(&signal1, "00001000");
+    bit_string::copy_bit_string_to_signal(&signal2, "00000011");
+
+    di_input_set(&a, signal1, simulation);
+    di_input_set(&b, signal2, simulation);
+
+    di_simulation_run(simulation, 100);
+
+    auto value = bit_string::signal_to_bit_string(&o.signal);
+
+    REQUIRE(value == "00011000");
+
+    di_simulation_free(simulation);
+
+    di_node_destroy(&aToMath);
+    di_node_destroy(&bToMath);
+    di_node_destroy(&mathToO);
+
+    di_input_destroy(&a);
+    di_input_destroy(&b);
+    di_arithmetic_destroy(&math);
+    di_output_destroy(&o);
+}
+
+TEST_CASE("Test Arithmetic Mul Signed") {
+    DiInput a;
+    DiInput b;
+
+    DiArithmetic math;
+
+    DiOutput o;
+
+    DiNode aToMath;
+    DiNode bToMath;
+
+    DiNode mathToO;
+
+    di_input_init(&a, 8);
+    di_input_init(&b, 8);
+    di_arithmetic_init(&math, 8, DI_ARITHMETIC_OP_MUL_SIGNED);
+    di_output_init(&o, 8);
+
+    di_node_init(&aToMath, 8);
+    di_node_init(&bToMath, 8);
+    di_node_init(&mathToO, 8);
+
+    di_connect(&aToMath, &a.output);
+    di_connect(&aToMath, &math.in_a);
+
+    di_connect(&bToMath, &b.output);
+    di_connect(&bToMath, &math.in_b);
+
+    di_connect(&mathToO, &math.output);
+    di_connect(&mathToO, &o.input);
+
+    DiSimulation *simulation = di_simulation_create();
+
+    DiSignal signal1;
+    DiSignal signal2;
+
+    di_signal_init(&signal1, 8);
+    di_signal_init(&signal2, 8);
+
+    bit_string::copy_bit_string_to_signal(&signal1, "11111110");
+    bit_string::copy_bit_string_to_signal(&signal2, "00000010");
+
+    di_input_set(&a, signal1, simulation);
+    di_input_set(&b, signal2, simulation);
+
+    di_simulation_run(simulation, 100);
+
+    auto value = bit_string::signal_to_bit_string(&o.signal);
+
+    REQUIRE(value == "11111100");
+
+    di_simulation_free(simulation);
+
+    di_node_destroy(&aToMath);
+    di_node_destroy(&bToMath);
+    di_node_destroy(&mathToO);
+
+    di_input_destroy(&a);
+    di_input_destroy(&b);
+    di_arithmetic_destroy(&math);
+    di_output_destroy(&o);
 }
